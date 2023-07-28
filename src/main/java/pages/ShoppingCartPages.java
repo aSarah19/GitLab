fccb0f967a325efa0afe5e1277deb2260ca173b9
Sarah <sarahantonie19@gmail.com>
2023-07-28 16:56:47 +0300

    shopping cart  commit

---
+++ src/main/java/pages/ShoppingCartPages.java
@@ -0,0 +1,102 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ShoppingCartPages extends BasePage{
+    public ShoppingCartPages(WebDriver driver) {
+        super(driver);
+    }
+
+    By ShoppingCartIcon = By.xpath("/html/body/div/div/div[1]/nav/div/div[2]/span/a[1]");
+    public void ClickOnShoppingCartToOpen() {driver.findElement(ShoppingCartIcon).click();}
+
+
+    By AddToCartAwesomeGraniteChips = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[1]");
+    public void ClickOnAddToCartAwesomeGraniteChips() {driver.findElement(AddToCartAwesomeGraniteChips).click();}
+
+
+    By AddToCartAwesomeMetalChair = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/div[2]/div/div[2]/button[1]");
+    public void ClickOnAddToCartAwesomeMetalChair() {driver.findElement(AddToCartAwesomeMetalChair).click();}
+
+
+    By AddToShopingCartIcon = By.xpath("/html/body/div/div/div[1]/nav/div/div[2]/span/a[1]");
+    public void ClickOnAddToShoppingCart() {driver.findElement(AddToShopingCartIcon).click();}
+
+
+    By AddMoreItemAwesomeGraniteChips = By.xpath("/html/body/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div/button[2]");
+    public void ClickOnAddMoreItemAwesomeGraniteChips() {driver.findElement(AddMoreItemAwesomeGraniteChips).click();}
+
+
+    By AddMoreItemAwesomeMetalChair = By.xpath("/html/body/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div[1]/div/button[2]");
+    public void ClickOnAddMoreItemAwesomeMetalChair() {driver.findElement(AddMoreItemAwesomeMetalChair).click();}
+
+    By LessItemAwesomeGraniteChips = By.xpath("/html/body/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div/button[1]");
+    public void ClickOnLessItemAwesomeGraniteChips() {driver.findElement(LessItemAwesomeGraniteChips).click();}
+
+
+    By AddGorgeousSoftPizzaToShoppingCart = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/div[4]/div/div[2]/button[1]");
+    public void ClickOnAddToCartGorgeousSoftPizza() {driver.findElement(AddGorgeousSoftPizzaToShoppingCart).click();}
+
+
+    By TrashIconGorgeousSoftPizza = By.xpath("/html/body/div/div/div[2]/div[2]/div[1]/div/div/div/div[5]/button");
+    public void ClickOnTrashIconGorgeousSoftPizza() {driver.findElement(TrashIconGorgeousSoftPizza).click();}
+
+
+    By GorgeousSoftPizza = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/div[4]/div/div[1]/a");
+    public void ClickOnGorgeousSoftPizza() {driver.findElement(GorgeousSoftPizza).click();}
+
+
+    By CheckoutButton = By.xpath("/html/body/div/div/div[2]/div[2]/div[3]/a[2]");
+    public void ClickOnCheckoutButton() {driver.findElement(CheckoutButton).click();}
+
+    By FirstName = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/input[1]");
+    public void ClickOnFirstName() {driver.findElement(FirstName).click();}
+
+    By FirstNameSarah = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/input[1]");
+    public void WriteFirstNameSarah() {driver.findElement(FirstNameSarah).click();}
+
+
+    By LastName = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/input[2]");
+    public void ClickOnLastName() {driver.findElement(LastName).click();}
+
+    By LastNameAntonie = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/input[2]");
+    public void WriteLastNameAntonie() {driver.findElement(LastName).click();}
+
+    By Address = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/textarea");
+    public void ClickOnAddress() {driver.findElement(Address).click();}
+
+    By WriteTheAddress = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[1]/div[2]/textarea");
+    public void WriteTheAddressSection() {driver.findElement(WriteTheAddress).click();}
+
+
+    By DeliveryInformation = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[2]/div[2]/div");
+    public void SelectDelivery() {driver.findElement(DeliveryInformation).click();}
+
+
+    By PaymentInformation = By.xpath("/html/body/div/div/div[2]/form/div/div[1]/div[2]/div[4]/div[1]");
+    public void SelectPaymentOption() {driver.findElement(PaymentInformation).click();}
+
+    By ContinueCheckoutButton = By.xpath("/html/body/div/div/div[2]/form/div/div[2]/button");
+    public void ClickOnContinueCheckoutButton() {driver.findElement(ContinueCheckoutButton).click();}
+
+    By CompleteYourOrder = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/a[2]");
+    public void ClickOnCompleteYourOrder() {driver.findElement(CompleteYourOrder).click();}
+
+    By ContinueShoppingButton = By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/a");
+    public void ClickOnContinueShoppingButton() {driver.findElement(ContinueShoppingButton).click();}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/testCases/ShoppingCartTestCases.java
@@ -0,0 +1,84 @@
+package testCases;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.DemoShopPage;
+import pages.ShoppingCartPages;
+
+public class ShoppingCartTestCases extends BasePage {
+    private ShoppingCartPages shoppingCartPages;
+
+    @BeforeClass
+    public void setUp() {
+        super.setUp();
+        shoppingCartPages = new ShoppingCartPages(driver);
+    }
+
+    @Test
+    public void ClickOnlyOnShoppingCart() throws InterruptedException{
+        shoppingCartPages.ClickOnShoppingCartToOpen();
+    }
+
+    @Test
+    public void ClickOnAddToCartItems() throws InterruptedException{
+        shoppingCartPages.ClickOnAddToCartAwesomeGraniteChips();
+        shoppingCartPages.ClickOnAddToCartAwesomeMetalChair();
+        shoppingCartPages.ClickOnAddToShoppingCart();
+        Assert.assertEquals("The item 1 and 2 are added to shopping cart." ,  "The item 1 and 2 are added to shopping cart.");
+    }
+
+
+    @Test
+    public void AddMoreItemsShoppingCart() throws InterruptedException{
+        shoppingCartPages.ClickOnAddToCartAwesomeGraniteChips();
+        shoppingCartPages.ClickOnAddToCartAwesomeMetalChair();
+        shoppingCartPages.ClickOnShoppingCartToOpen();
+        shoppingCartPages.ClickOnAddMoreItemAwesomeGraniteChips();
+        shoppingCartPages.ClickOnAddMoreItemAwesomeMetalChair();
+        Assert.assertEquals("The item is successful added." , "The item is successful added.");
+    }
+
+
+    @Test
+    public void LessItemsShoppingCart() throws InterruptedException{
+        shoppingCartPages.ClickOnAddToCartAwesomeGraniteChips();
+        shoppingCartPages.ClickOnShoppingCartToOpen();
+        shoppingCartPages.ClickOnLessItemAwesomeGraniteChips();
+
+    }
+
+
+    @Test
+    public void TrashIconDeleteItem() throws InterruptedException{
+        shoppingCartPages.ClickOnAddToCartGorgeousSoftPizza();
+        shoppingCartPages.ClickOnShoppingCartToOpen();
+        shoppingCartPages.ClickOnTrashIconGorgeousSoftPizza();
+        Assert.assertEquals("The item is removed by trash icon." , "The item is removed by trash icon.");
+    }
+
+
+    @Test
+    public void CompleteYourOrder() throws InterruptedException{
+        shoppingCartPages.ClickOnGorgeousSoftPizza();
+        shoppingCartPages.ClickOnAddToCartGorgeousSoftPizza();
+        shoppingCartPages.ClickOnShoppingCartToOpen();
+        shoppingCartPages.ClickOnCheckoutButton();
+        shoppingCartPages.ClickOnFirstName();
+        shoppingCartPages.WriteFirstNameSarah();
+        shoppingCartPages.ClickOnLastName();
+        shoppingCartPages.WriteLastNameAntonie();
+        shoppingCartPages.ClickOnAddress();
+        shoppingCartPages.WriteTheAddressSection();
+        shoppingCartPages.SelectDelivery();
+        shoppingCartPages.SelectPaymentOption();
+        shoppingCartPages.ClickOnContinueCheckoutButton();
+        shoppingCartPages.ClickOnCompleteYourOrder();
+        shoppingCartPages.ClickOnContinueShoppingButton();
+
+
+
+    }
+
+
+}
